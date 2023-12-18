c767bd44e41fd95955a35bc5e30b959f5215c81a
akazakov21 <131651247+akazakov21@users.noreply.github.com>
2023-12-18 08:31:52 +0200

    TestSth (#6)
    Co-authored-by: Angel <mgplovdiv@outlook.com>

---
+++ PersonEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class PersonEntity
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+    public string Address { get; set; }
+
+    public PersonEntity(string firstName, string lastName, DateTime birthDate, string address)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        BirthDate = birthDate;
+        Address = address;
+    }
+    public int CalculatePersonAge()
+    {
+        int age = DateTime.Now.Year - BirthDate.Year;
+        if (DateTime.Now < BirthDate.AddYears(age)) age--;
+        return age;
+    }
+
+    public virtual void DisplayPersonInfo()
+    {
+        Console.WriteLine($"Name: {FirstName} {LastName}");
+        Console.WriteLine($"Birth Date: {BirthDate.ToShortDateString()}");
+        Console.WriteLine($"Age: {CalculatePersonAge()} years old");
+        Console.WriteLine($"Address: {Address}");
+    }
+}
